d47667ad1a9b2149c78fd67f38bf3b98284bbe0c
hassanbaryal <h.baryal2@gmail.com>
2022-03-01 15:37:20 -0500

    updated script.js

--- script.js
+++ script.js
@@ -7,24 +7,49 @@ function computerPlay(playerSelection, computerSelection) {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = (prompt('Please select your champion! (Rock, paper, or scissor)')).toLowerCase();
-    computerSelection = computerPlay();
+    
+    let winCondition = false;
 
-    if (playerSelection == 'rock') {
-
-    } else if (playerSelection == 'paper') {
-
-    } else if (playerSelection == 'scissor'){
-
-    } else {
-        alert('please select a valid option! (Rock, paper, or scissor');
-        playRound();
+    if (playerSelection == computerSelection) {
+        alert(`Its a tie! You both chose ${playerSelection}`);
+        return;
+    } else if ( (playerSelection == 'rock' && computerSelection == 'scissor') ||
+    (playerSelection == 'paper' && computerSelection == 'rock') ||
+    (playerSelection == 'scissor' && computerSelection == 'paper')) {
+        winCondition = true;
     }
 
-    return;
+    if (winCondition) {
+        alert(`Woah, ${playerSelection} beats ${computerSelection}. You won!!`);
+    } else {
+        alert(`Damn, ${playerSelection} loses to ${computerSelection}. You lost.`)
+    }
+
+    return winCondition;
 }
 
 
+
+function game() {
+    let playerSelection;
+    let computerSelection;
+    let wins = 0
+    let losses = 0;
+    alert('Rock, paper, scissor against the computer! This is a best of 5.')
+    for (let i = 1; i < 6; i++){
+        alert(`Commencing Round ${i}!`);
+        while (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissor') {
+            playerSelection = (prompt('Please select your champion! (Rock, paper, or scissor)')).toLowerCase();
+        }
+        computerSelection = computerPlay();
+
+        
+
+    }
+
+
+    return;
+}
 
 console.log(prompt())
 console.log(computerPlay())
